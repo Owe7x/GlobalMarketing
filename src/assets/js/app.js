6aacc37f35058f47d1bd133dfb41d89a1d476aa8
Timur <thedickst@gmail.com>
2021-05-28 14:30:22 +0300

    Fix

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,7 +12,7 @@ function sliderHandler(price) {
       price = srvPrices;
     }
  
-    var persents = (price.totalS1 * 11300) + (price.totalS2 * 1000) + (price.totalS3 * 25000) + (price.totalS4 * 10000);
+    var persents = (price.totalS1 * 12000) + (price.totalS2 * 1070) + (price.totalS3 * 40000) + (price.totalS4 * 21000);
     var profit = persents * 0.3;
      document.getElementById("s1").innerHTML = persents.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + ' ' + 'рублей';
      document.getElementById("s2").innerHTML = profit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + ' ' + 'рублей';
@@ -26,7 +26,7 @@ function sliderHandler(price) {
   $(".range1").ionRangeSlider({
     from: 3,
     grid: false,
-    values: [0,10,20,30,40,50],
+    values: [5,7,10,13,15],
     onChange: function (data) {
       srvPrices.totalS1 = data.from_value;
       sliderHandler(srvPrices)
@@ -39,7 +39,7 @@ function sliderHandler(price) {
  $(".range2").ionRangeSlider({
   from: 2,
   grid: false,
-  values: [0,50,100,150,250,350],
+  values: [30,50,70,90,110],
   onChange: function (data) {
     srvPrices.totalS2 = data.from_value;
     sliderHandler(srvPrices)
@@ -52,7 +52,7 @@ function sliderHandler(price) {
 $(".range3").ionRangeSlider({
   from: 5,
   grid: false,
-  values: [0,5,10,20,30,40],
+  values: [10,14,18,22,26],
   onChange: function (data) {
     srvPrices.totalS3 = data.from_value;
     sliderHandler(srvPrices)
@@ -65,7 +65,7 @@ $(".range3").ionRangeSlider({
 $(".range4").ionRangeSlider({
   from: 10,
   grid: false,
-  values: [0,10,20,30,40,50],
+  values: [2,4,6,8,10],
   onChange: function (data) {
     srvPrices.totalS4 = data.from_value;
     sliderHandler(srvPrices)
